a4abb8de2de2e5a9239216d5d8a858e78a45bc4c
gunarto90 <gunarto.nctu@gmail.com>
2016-11-18 11:38:53 +0800

    Extract all apps in each activity + stop words concept

--- view_soft.py
+++ view_soft.py
@@ -9,22 +9,6 @@ from datetime import datetime
 from string import digits
 from general import *
 
-def init(file='setting.json'):
-    global dataset_folder
-    global working_folder
-    global user_ids
-    global activities
-    try:
-        with open(file) as data_file:
-            data = json.load(data_file)
-            ### Extracting variables
-            dataset_folder = data[st.get_dataset_folder()]
-            working_folder = data[st.get_working_folder()]
-            user_ids = data[st.get_uids()]
-            activities = data[st.get_activities()]
-    except Exception as ex:
-        debug(ex, callerid='init - json')
-
 # Main function
 if __name__ == '__main__':
     ### Load options (if any)
@@ -37,7 +21,7 @@ if __name__ == '__main__':
     # if len(opts) > 0:
     #     pass
     ### Initialize variables from json file
-    init()
+    dataset_folder, working_folder, user_ids, activities = init()
     debug(dataset_folder)
     debug(user_ids)
     apps_agg = []
@@ -70,32 +54,81 @@ if __name__ == '__main__':
                         found = []
                     found.append(act_int)
                     apps_single[app_id] = found
+    ### Time
     # aa = np.array(activity_hour)
     # debug(aa.shape)
     # debug(aa.T.shape)
-    for i in range(len(aa.T)):
-        ent = entropy(aa.T[i])
-        debug('{},{}'.format(i, ent), clean=True)
+    # for i in range(len(aa.T)):
+    #     ent = entropy(aa.T[i])
+    #     debug('{},{}'.format(i, ent), clean=True)
     # for i in range(len(activities)):
     #     debug(activity_hour[i], clean=True)
-    # debug(len(apps_agg))
-    # debug(len(apps_single))
-    # debug(activities)
-    # acts = []
-    # for i in range(len(activities)):
-    #     acts.append(0)
-    # # debug(acts)
-    # for app_id, uacts in apps_single.items():
-    #     # debug(app_id)
-    #     # debug(len(uacts))
-    #     for a in uacts:
-    #         acts[a] += 1
-    #     # debug(len(acts))
-    #     # debug(entropy(acts))
-    #     text = '{},{},{}'.format(app_id, len(uacts), entropy(acts))
-    #     debug(text, clean=True)
-    #     ### Clean array in every loop
-    #     del acts[:]
-    #     for i in range(len(activities)):
-    #         acts.append(0)
-    #     # break
+    ### Apps
+    debug(len(apps_agg))
+    debug(len(apps_single))
+    debug(activities)
+    acts = []
+    acts_app = []
+    app_stats = {}
+    stop_words = ['com', 'google', 'example', 'android', 'htc', 'samsung', 'sony', 'unstable', 'process', 'systemui', 'system', 'nctuhtclogger']
+    for i in range(len(activities)):
+        acts.append(0)
+        acts_app.append({})
+    # debug(acts)
+    for app_id, uacts in apps_single.items():
+        if app_id in stop_words:
+            continue
+        # debug(app_id)
+        # debug(len(uacts))
+        for a in uacts:
+            acts[a] += 1
+            acts_map = acts_app[a]
+            found = acts_map.get(app_id)
+            if found is None:
+                found = {'f':0, 'e':0.0}
+            found['f'] += 1
+            acts_map[app_id] = found
+        # debug(len(acts))
+        # debug(entropy(acts))
+        text = '{},{},{},{}'.format(app_id, len(uacts), entropy(acts, len(activities)), acts)
+        app_stats[app_id] = (len(uacts), entropy(acts, len(activities)))
+        # debug(text, clean=True)
+        ### Clean array in every loop
+        del acts[:]
+        for i in range(len(activities)):
+            acts.append(0)
+    for app_id, (f, e) in app_stats.items():
+        for i in range(len(activities)):
+            acts_map = acts_app[i]
+            found = acts_map.get(app_id)
+            if found is None:
+                found = {'f':0, 'e':0.0}
+            total = sum(acts_map[x]['f'] for x in acts_map)
+            if total > 0:
+                found['f'] = float(found['f'])/total
+            else:
+                found['f'] = 0.0
+            found['e'] = e
+            if found['f'] > 0 and found['e'] > 0:
+                acts_map[app_id] = found
+    for i in range(len(activities)):
+        topk = 3
+        k = 0
+        debug(activities[i])
+        debug(acts_app[i])
+        # ### Sorting by 1-entropy * frequency (descending)
+        # for app_id, value in sorted(acts_app[i].items(), key=lambda value: (1-value[1]['e'])*value[1]['f'], reverse=True):
+        #     if k >= topk:
+        #         break
+        #     debug('{},{},{}'.format(app_id, value['e'], value['f']), clean=True)
+        #     k += 1
+        # debug('---')
+        # ### Sorting by frequency (descending)
+        # for app_id, value in sorted(acts_app[i].items(), key=lambda value: value[1]['f'], reverse=True):
+        #     if k >= topk:
+        #         break
+        #     # debug('{},{},{}'.format(app_id, value['e'], value['f']), clean=True)
+        #     debug(app_id)
+        #     debug(value)
+        #     k += 1
+        # debug('---')
