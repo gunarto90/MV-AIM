8a4229635e590df14b454c84d005941091e1175b
gunarto90 <gunarto.nctu@gmail.com>
2016-11-24 16:59:19 +0800

    Only use accuracy for now

--- evaluation.py
+++ evaluation.py
@@ -2,7 +2,7 @@
 Code by Gunarto Sindoro Njoo
 Written in Python 3.5.2 (Anaconda 4.1.1) -- 64bit
 Version 1.0.0
-2016/11/24 04.45PM
+2016/11/24 04.53PM
 """
 from general import *
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
@@ -58,43 +58,39 @@ def evaluation(X, y, clf, k_fold=5):
         probas_ = fit.predict_proba(X[test])
         inference = fit.predict(X[test])
         # Compute ROC curve and area the curve
-        fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
-        mean_tpr += interp(mean_fpr, fpr, tpr)
-        mean_tpr[0] = 0.0
-        roc_auc = auc(fpr, tpr)
+        # fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+        # mean_tpr += interp(mean_fpr, fpr, tpr)
+        # mean_tpr[0] = 0.0
+        # roc_auc = auc(fpr, tpr)
 
         # precision, recall, thresholds = precision_recall_curve(y[test], probas_[:, 1])
         average = 'weighted'
-        precision = precision_score(y[test], inference, average=average)
-        recall = recall_score(y[test], inference, average=average)
-        f1 = f1_score(y[test], inference, average=average)
+        # precision = precision_score(y[test], inference, average=average)
+        # recall = recall_score(y[test], inference, average=average)
+        # f1 = f1_score(y[test], inference, average=average)
         acc = accuracy_score(y[test], inference)
 
-        mean_precision += precision
-        mean_recall += recall
-        mean_f1 += f1
+        # mean_precision += precision
+        # mean_recall += recall
+        # mean_f1 += f1
         mean_acc += acc
         # debug(roc_auc)
         i += 1
-    mean_tpr /= cv.get_n_splits(X, y)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
+    # mean_tpr /= cv.get_n_splits(X, y)
+    # mean_tpr[-1] = 1.0
+    # mean_auc = auc(mean_fpr, mean_tpr)
 
     n_split = cv.get_n_splits(X, y)
-    mean_precision /= n_split
-    mean_recall /= n_split
-    mean_f1 /= n_split
+    # mean_precision /= n_split
+    # mean_recall /= n_split
+    # mean_f1 /= n_split
     mean_acc /= n_split
 
-    output['auc']   = mean_auc
-    output['p']     = mean_precision
-    output['r']     = mean_recall
-    output['f1']    = mean_f1
+    # output['auc']   = mean_auc
+    # output['p']     = mean_precision
+    # output['r']     = mean_recall
+    # output['f1']    = mean_f1
     output['acc']   = mean_acc
-    output['y1']    = total_ytrue
+    # output['y1']    = total_ytrue
 
     return output
-
-# Main function
-if __name__ == '__main__':
-    pass
