abc57a80b32edc6b3aceafdc27b870658d55a58c
gunarto90 <gunarto.nctu@gmail.com>
2016-11-28 09:42:26 +0800

    Modified config files

---
+++ config_directory.py
@@ -0,0 +1,16 @@
+"""
+Code by Gunarto Sindoro Njoo
+Written in Python 3.5.2 (Anaconda 4.1.1) -- 64bit
+Version 1.0
+2016/11/28 09:25AM
+"""
+
+"""
+No logic code here !
+This file just declares the variables that are used throughout the other python files.
+"""
+
+dataset_folder  = 'dataset/'
+working_folder  = 'working/'
+log_folder      = 'log/'
+model_folder    = 'model/'
